71890a1a01638824bd019b6683159651a1642b8f
dipayancodes <krishnadipayan108@gmail.com>
2023-01-25 15:05:01 +0530

    css file has changed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// console.log("hare krishna" )
+// ======================== HARE KRISHNA ============================
 
 const barberian = document.getElementById("barberian")
 const archer = document.getElementById("archer")
@@ -17,54 +17,64 @@ dcount = 0
 
 barberian.onclick = () => {
   bcount += 1
-  barberian.innerHTML = "barberian:" + bcount.toString(10);
+  barberian.innerHTML = "barberian:" + bcount;
 }
 
 archer.onclick = () => {
   acount += 1
-  archer.innerHTML = "archer:" + acount.toString(10);
+  archer.innerHTML = "archer:" + acount;
 }
 
 giant.onclick = () => {
   gcount += 1
-  giant.innerHTML = "giant:" + gcount.toString(10);
+  giant.innerHTML = "giant:" + gcount;
 }
 
 wizard.onclick = () => {
   wcount += 1
-  wizard.innerHTML = "wizard:" + wcount.toString(10);
+  wizard.innerHTML = "wizard:" + wcount;
 }
 
 dragon.onclick = () => {
   dcount += 1
-  dragon.innerHTML = "dragon:" + dcount.toString(10);
+  dragon.innerHTML = "dragon:" + dcount;
 }
 
-checkifnotzero = parseInt(bcount+acount+gcount+wcount+dcount)
 
-result.addEventListener("click",() => {
-  if (checkifnotzero === 0) {
-    console.log("train some troop first")
+result.addEventListener("click", () => {
+  troopstrained = bcount+acount+gcount+wcount+dcount
+  console.log("you trained " + bcount + " ammount of barberian")
+  console.log("you trained " + acount + " ammount of archer")
+  console.log("you trained " + gcount + " ammount of giant")
+  console.log("you trained " + wcount + " ammount of wizard")
+  console.log("you trained " + dcount + " ammount of dragon")
+  landedtroops = console.log("you started attacking with " + troopstrained + " ammount of troops")
+  if (troopstrained === 0) {
+    console.log("train some troops first")
   } else {
-   landedTroops = console.log("you started attacking with "+ checkifnotzero + " troops")
-   setTimeout(() => {
-      console.log("started batteling ⚔....") 
-   }, 3000);
-  setTimeout(() => {
+    setTimeout(() => {
+      console.log("Finding a Battle for you.....") 
+    }, 5000);
+    setTimeout(() => {
+      console.log("FOUND A BATTLE ⚔") 
+    }, 5000);
+    setTimeout(() => {
+      console.log("Batteling with your oponnentx...") 
+    }, 9000);
+
+
+    wonlos = ["won", "lost"]
+    wonlosresult = Math.floor(Math.random(wonlos))
     stars = Math.floor(Math.random() * 3)
     acc = Math.ceil(Math.random() * 100)
-    if (stars >= 1) {
-      console.log("you won " + stars + " stars with " + acc + "% accuracy")
+
+    if (stars === 0) {
+      console.log("you LOST better luck next time :(")
     } else {
-      console.log("you lost " + stars + " stars with " + acc + "% accuracy")
+      setTimeout(() => {
+        console.log("you got WON " + stars + " stars with " + acc + "% accuracy") 
+      }, 10000);
     }
-  }, 5000);
   } 
-
-
-
-
 })
-
-
 
